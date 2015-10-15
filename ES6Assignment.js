2fb1a0c30fc10ef8088919796cd1fd8c0c6c41c0
RehanaVanSchalkwyk <rehanav@nvisionit.co.za>
2015-10-15 14:07:23 +0200

    Added Arrow Functions and Destructing

--- ES6Assignment.js
+++ ES6Assignment.js
@@ -13,7 +13,7 @@
 	
 	speak()
 	{
-		alert("Hi my name is " + name) ; 
+		console.log("Hi my name is " + name". ") ; 
 	}
 	 
  }
@@ -31,14 +31,68 @@
 		//calls Animal.speak()
 		super.speak()//
 		
-		alert("I am a bird and I chirp.");
+		console.log("I am a bird and I chirp.");
 	}
  }
  
  var budgee = new Bird("Tweety");
- budgee.speak();
+ budgee.speak(); //Hi my name is Tweety. I am a bird and I chirp.
  
  
  //ES6 Arrow Functions
- 
- 
+var animals = [
+{type: 'bird', sound: 'chrip' },
+{type: 'lion', sound: 'roar' },
+{type: 'snake',sound: 'snake' }
+];
+
+console.log(animals.map(animal => animal.name));//bird lion snake
+
+//Lexical this, Arrow functions 
+function Animal(){
+  this.age = 0;
+
+  setInterval(() => {
+    this.age++; // |this| properly refers to the Animal object
+  }, 1000);
+}
+
+var a = new Animal();
+
+//object literals
+var checkAge = a => a > 18 ? 18 : a; 
+console.log(checkAge(21)); // 18
+console.log(checkAge(12); // 12
+
+
+var numbers = [1,2,3,4,5];
+var sum = numbers.reduce((total, num) => total + num);
+console.log(sum); // 15
+
+ //ES6 Destructuring
+ //Array Destructuring
+var nums = ["one", "two", "three"];
+var [one, two, three] = nums;
+
+//swapping
+var a = 1;
+var b = 3;
+
+[a, b] = [b, a];
+
+//Multiple Value return
+function f() {
+  return [1, 2];
+}
+
+var a, b;
+[a, b] = f();
+console.log("A is " + a + " B is " + b);// A is 1 B is 2
+
+
+function ignore() {
+  return [1, 2, 3];
+}
+
+var [a, , b] = ignore();
+console.log("A is " + a + " B is " + b);//A is 1 and b is 3.
