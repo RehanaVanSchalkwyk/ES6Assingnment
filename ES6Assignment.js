8ef90ee84f5333edb89717a111b3c95e79c2ae95
RehanaVanSchalkwyk <rehanav@nvisionit.co.za>
2015-10-15 15:21:56 +0200

    Added the rest of destructuring and Added spread operator

--- ES6Assignment.js
+++ ES6Assignment.js
@@ -2,7 +2,8 @@
  ES6  Assignment*/  
  
  
- // ES6 Classes
+/*-------------------------------------------ES6 Classes----------------------------------------------------------*/
+ //Inheritance
  class Animal
  {
 	 constructor(name)
@@ -39,7 +40,7 @@
  budgee.speak(); //Hi my name is Tweety. I am a bird and I chirp.
  
  
- //ES6 Arrow Functions
+/*-------------------------------------------ES6 Arrow Functions--------------------------------------------------*/
 var animals = [
 {type: 'bird', sound: 'chrip' },
 {type: 'lion', sound: 'roar' },
@@ -69,7 +70,7 @@ var numbers = [1,2,3,4,5];
 var sum = numbers.reduce((total, num) => total + num);
 console.log(sum); // 15
 
- //ES6 Destructuring
+/*-------------------------------------------ES6 Destructuring----------------------------------------------------*/
  //Array Destructuring
 var nums = ["one", "two", "three"];
 var [one, two, three] = nums;
@@ -96,3 +97,33 @@ function ignore() {
 
 var [a, , b] = ignore();
 console.log("A is " + a + " B is " + b);//A is 1 and b is 3.
+
+// Object Destructuring
+var ob = {a: true, b: 24};
+var {a, b} = ob;
+
+console.log(a); // true
+console.log(b); // 24 
+
+//Assign new variable names
+var {a: obj1, b: obj2} = ob;
+
+console.log(obj1); // 42
+console.log(obj2); // true  
+
+//object property names and destructuring
+let key = "d";
+let { [key]: magicWord } = { d: "open" };
+
+console.log(magicWord); // "open"
+
+/*-------------------------------------------ES6 Spread operator, rest parameters---------------------------------*/
+
+var missingParts = ['two', 'three'];
+var numberList = ['one', ...missingParts, 'four', 'five']; 
+console.log(numberList)// one, two, three, four, five
+
+var arr1 = [0, 1, 2];
+var arr2 = [3, 4, 5];
+arr1.push(...arr2);
+console.log(arr1); // 012345
